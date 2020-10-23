b3fe4f68382b19d882809fd5eaa30f21fe463248
xiangfenr <xiangfenr>
2020-10-23 16:25:22 +0800

    交易记录和清机记录ok

---
+++ app/src/main/java/com/licheedev/serialtool/activity/manage/print/ClearingRecordActivity.java
@@ -0,0 +1,98 @@
+package com.licheedev.serialtool.activity.manage.print;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.licheedev.serialtool.R;
+import com.licheedev.serialtool.activity.base.BaseActivity;
+import com.licheedev.serialtool.activity.dapter.BaseRecyclerAdapter;
+import com.licheedev.serialtool.activity.dapter.RecyclerViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * 清机记录
+ */
+public class ClearingRecordActivity extends BaseActivity implements BaseRecyclerAdapter.Delegate {
+
+    @BindView(R.id.recyclerview)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.btLogout)
+    Button btnUpload;
+
+
+    private BaseRecyclerAdapter adapter;
+    private List<Integer> list = new ArrayList<>();
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_clearing_record;
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+        btnUpload.setText(getResources().getString(R.string.print));
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(manager);
+        adapter = new BaseRecyclerAdapter(this, this);
+        mRecyclerView.setAdapter(adapter);
+
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        adapter.notifyDataSetChanged();
+    }
+
+    @OnClick({R.id.btnBack,  R.id.btLogout})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnBack:
+                finish();
+                break;
+
+            case R.id.btLogout:
+
+                break;
+        }
+    }
+
+    @Override
+    public List<Integer> getData() {
+        return list;
+    }
+
+    @Override
+    public int getView(ViewGroup parent, int viewType) {
+        return R.layout.item_clearing_record;
+    }
+
+    @Override
+    public <T> void bindView(RecyclerViewHolder holder, int position, T item) {
+        View viewTop = holder.getView(R.id.viewTop);
+        View viewBottom = holder.getView(R.id.viewBottom);
+        if (position == 0) {
+            viewTop.setVisibility(View.GONE);
+        } else {
+            viewTop.setVisibility(View.VISIBLE);
+        }
+
+        if (position == list.size() - 1) {
+            viewBottom.setVisibility(View.VISIBLE);
+        } else {
+            viewBottom.setVisibility(View.GONE);
+        }
+
+    }
+}
--- app/src/main/java/com/licheedev/serialtool/activity/manage/print/SystemPrintActivity.java
+++ app/src/main/java/com/licheedev/serialtool/activity/manage/print/SystemPrintActivity.java
@@ -1,9 +1,12 @@
 package com.licheedev.serialtool.activity.manage.print;
 
+import android.content.Intent;
 import android.view.View;
 
 import com.licheedev.serialtool.R;
 import com.licheedev.serialtool.activity.base.BaseActivity;
+import com.licheedev.serialtool.activity.deposit.DepositManageActivity;
+import com.licheedev.serialtool.activity.deposit.DepositRecordActivity;
 
 import butterknife.OnClick;
 
@@ -21,12 +24,16 @@ public class SystemPrintActivity extends BaseActivity {
     @OnClick({R.id.tvcreenbill, R.id.tvclearrecord, R.id.tvdepositrecored, R.id.btnBack, R.id.btLogout})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.tv_depositdata:
+            case R.id.tvcreenbill:
                 break;
 
-            case R.id.tv_bill_data:
+            case R.id.tvclearrecord:
+                startActivity(new Intent(this, ClearingRecordActivity.class));
                 break;
 
+            case R.id.tvdepositrecored: //存款记录
+                startActivity(new Intent(this, DealRecordActivity.class));
+                break;
 
             case R.id.btnBack:
                 finish();
